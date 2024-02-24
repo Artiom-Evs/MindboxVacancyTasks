cc63a95c29652c28a34347ec4b0d1c5c4a843f01
Artiom-Evs <artiom.evs@gmail.com>
2024-02-24 18:19:00 +0100

    implement area calculation tests for abstract shapes

---
+++ MindboxVacancyTasks.Task1.Tests/GeneralTests.cs
@@ -0,0 +1,26 @@
+﻿
+using MindboxVacancyTasks.Task1.Models;
+
+namespace MindboxVacancyTasks.Task1.Tests;
+
+public class GeneralTests
+{
+    [Test]
+    public void TestAreaCalculationForAbstractShapes()
+    {
+        ShapeBase[] shapes =
+        [
+            new Triangle(3, 4, 5),
+            new Circle(10)
+        ];
+        double[] areas =
+        [
+            6,
+            314.15926535897933d
+        ];
+
+        double[] results = shapes.Select(s => s.CalculateArea()).ToArray();
+
+        Assert.That(results, Is.EqualTo(areas));
+    }
+}
