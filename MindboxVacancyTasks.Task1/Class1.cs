31241c428c5e01f667cb2087dec3fd72eee3e7c5
Artiom-Evs <artiom.evs@gmail.com>
2024-02-24 17:04:17 +0100

    Add ShapeBase abstract class
    #This class represents abstract shape with area calculation functionality.

--- MindboxVacancyTasks.Task1/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace MindboxVacancyTasks.Task1
-{
-    public class Class1
-    {
-
-    }
-}
---
+++ MindboxVacancyTasks.Task1/Models/ShapeBase.cs
@@ -0,0 +1,6 @@
+﻿namespace MindboxVacancyTasks.Task1.Models;
+
+public abstract class ShapeBase
+{
+    public abstract double CalculateArea();
+}
