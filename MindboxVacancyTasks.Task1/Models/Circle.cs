8e52a2b32f2ea33fa78e6930139f555ef0ba7366
Artiom-Evs <artiom.evs@gmail.com>
2024-02-24 17:25:24 +0100

    Add Circle class
    This class represents circle shape and inherited from ShapeBase abstract class.

---
+++ MindboxVacancyTasks.Task1/Models/Circle.cs
@@ -0,0 +1,17 @@
+﻿
+namespace MindboxVacancyTasks.Task1.Models;
+
+public class Circle : ShapeBase
+{
+    public Circle(double radius)
+    {
+        Radius = radius;
+    }
+
+    public double Radius { get; init; }
+
+    public override double CalculateArea()
+    {
+        return Math.PI * Radius * Radius;
+    }
+}
