0a435c7d40c909813ac19fb02b05ccc1d62cec41
Artiom-Evs <artiom.evs@gmail.com>
2024-02-24 17:25:48 +0100

    Implement circle area calculation tests

---
+++ MindboxVacancyTasks.Task1.Tests/CircleTests.cs
@@ -0,0 +1,16 @@
+﻿using MindboxVacancyTasks.Task1.Models;
+
+namespace MindboxVacancyTasks.Task1.Tests;
+
+public class CircleTests
+{
+    [TestCase(10, 314.15926535897933d)]
+    public void CalculateCircleArea(double radius, double expected)
+    {
+        Circle circ = new(radius);
+
+        double result = circ.CalculateArea();
+
+        Assert.That(result, Is.EqualTo(expected));
+    }
+}
