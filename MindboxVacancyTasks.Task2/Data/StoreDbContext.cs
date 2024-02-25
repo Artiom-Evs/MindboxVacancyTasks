dffedd7e41df0c520694a09d3070e2c57ec582e7
Artiom-Evs <artiom.evs@gmail.com>
2024-02-25 01:28:31 +0100

    Add StoreDbContext class
    This class represents data access layer for for SQLite database.

---
+++ MindboxVacancyTasks.Task2/Data/StoreDbContext.cs
@@ -0,0 +1,24 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using MindboxVacancyTasks.Task2.Models;
+
+namespace MindboxVacancyTasks.Task2.Data;
+
+public class StoreDbContext : DbContext
+{
+    public StoreDbContext()
+    {
+        Database.EnsureDeleted();
+        Database.EnsureCreated();
+    }
+
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Category> Categories { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        string connectionString = @"Filename=""store.db""";
+            
+        optionsBuilder.UseSqlite(connectionString);
+    }
+}
