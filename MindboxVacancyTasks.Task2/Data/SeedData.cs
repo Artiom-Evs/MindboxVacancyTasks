0986c3c806cac7341bc595daef7b87f8ec0687cd
Artiom-Evs <artiom.evs@gmail.com>
2024-02-25 01:30:35 +0100

    Add SeedData class
    This class is designed to insert initial data into the database.

---
+++ MindboxVacancyTasks.Task2/Data/SeedData.cs
@@ -0,0 +1,29 @@
+﻿
+using MindboxVacancyTasks.Task2.Models;
+
+namespace MindboxVacancyTasks.Task2.Data;
+
+public static class SeedData
+{
+    public static void EnsurePopulated(StoreDbContext context)
+    {
+        Product[] products =
+        [
+            new() { Name = "Laptop" },
+            new() { Name = "Printer" },
+            new() { Name = "Pineapple" }
+        ];
+        Category[] categories =
+        [
+            new() { Name = "Electronics" },
+            new() { Name = "Office equipment" }
+        ];
+
+        products[0].Categories.Add(categories[0]);
+        products[1].Categories.Add(categories[0]);
+        products[1].Categories.Add(categories[1]);
+
+        context.Products.AddRange(products);
+        context.SaveChanges();
+    }
+}
